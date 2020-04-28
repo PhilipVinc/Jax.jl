641fc1c5f28202bbeb938c4b563df8ebb017499e
Filippo Vicentini <filippovicentini@gmail.com>
2020-04-28 23:43:20 +0200

    wrappers

---
+++ src/lib/array.jl
@@ -0,0 +1,10 @@
+Base.transpose(a::AbstractJaxVector) = numpy.reshape(a, (-1,1))
+Base.transpose(a::AbstractJaxMatrix) = numpy.transpose(a)
+
+Base.adjoint(a::AbstractJaxArray{T}) where T<:Real = transpose(a)
+Base.adjoint(a::AbstractJaxArray) = conj(transpose(a))
+
+Base.PermutedDimsArray(a::AbstractJaxArray, perm) = permutedims(a, perm)
+Base.permutedims(a::AbstractJaxArray) = transpose(a)
+Base.permutedims(a::AbstractJaxArray{T,N}, perm::Dims{N}) where {T,N} =
+    numpy.transpose(a, reverse(N.-perm))
